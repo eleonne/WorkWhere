b8a627216a0e18a77e64159d6205edb7b0ed77da
Eduardo Leonne <chamascadentes@gmail.com>
2026-03-05 00:34:06 -0500

    new configs for prod

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import cors from 'cors'
 import { teleworkRouter } from './routes/telework-routes'
@@ -8,5 +9,13 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/telework', teleworkRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  const distPath = path.join(__dirname, '../dist')
+  app.use(express.static(distPath))
+  app.use((_req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'))
+  })
+}
 
 export { app }
